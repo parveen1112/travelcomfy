c85ad4a36410e2e747e490799a81dc48d45c3471
SAPIENT\paro28 <parora28@sapient.com>
2017-05-27 01:36:12 +0530

    Unit Test

---
+++ assets/js/app/config.js
@@ -0,0 +1,9 @@
+define(function(){
+    return {
+        searchUrl : '/search/',
+        passStr : 'seats',
+        mnStr : 'mn',
+        mxStr : 'mx',
+        noResultFlight : 'Sorry!! No Flight Available'
+    };
+});
